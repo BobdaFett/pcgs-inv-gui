7153eee6ef114d83c35a108ec4dd62294349d107
BobdaFett <lvas463@gmail.com>
2023-07-16 13:25:54 -0400

    no idea rn

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='pcgs-inv-gui',
+    version='0.1a0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/bobdafett/pcgs-inv-gui',
+    license='',
+    author='Lucas Vas',
+    author_email='lvas463@gmail.com',
+    description='PCGS Coin Inventory'
+)
