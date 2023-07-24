652c7179e0812a85eaf38763e4ba9117ebbe81cc
BobdaFett <lvas463@gmail.com>
2023-07-24 10:42:58 -0400

    Edited the CoinCollection to add iteration.

--- src/api_utils.py
+++ src/api_utils.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem
+from collections.abc import Sequence
 
 import requests
 import json
@@ -10,14 +11,14 @@ class PCGSClient:
         self.API_KEY = api_key
 
     def request_facts_by_grade(self, pcgs: int, grade: int, plus_grade: bool=False) -> dict:
-        ''' Handles sending a request to the PCGS Public API. Returns a JSON serialized value. '''
+        ''' Handles sending a request to the PCGS Public API. Returns a JSON deserialized value. '''
         request_url = self.API_URL + "/coindetail/GetCoinFactsByGrade/?PCGSNo={0}&GradeNo={1}&PlusGrade={2}".format(pcgs, grade, plus_grade)
         result = requests.get(request_url, headers={'authorization': 'bearer ' + self.API_KEY})
         result.raise_for_status()
         return result.json()
 
     def request_facts_by_barcode(self, barcode: int, service: str) -> dict:
-        ''' Handles sending a request to the PCGS Public API. Returns a JSON serialized value. 
+        ''' Handles sending a request to the PCGS Public API. Returns a JSON deserialized value. 
             Note that the service argument only accepts PCGS or NGC. '''
         request_url = self.API_URL + "/coindetail/GetCoinFactsByBarcode/?barcode={0}&gradingService={1}".format(barcode, service)
         result = requests.get(request_url, headers={'authorization': 'bearer ' + self.API_KEY})
@@ -25,7 +26,7 @@ class PCGSClient:
         return result.json()
 
     def request_facts_by_cert(self, cert_number: int) -> dict:
-        ''' Handles sending a request to the PCGS Public API. Returns a JSON serialized value. '''
+        ''' Handles sending a request to the PCGS Public API. Returns a JSON deserialized value. '''
         request_url = self.API_URL + "/coindetail/GetCoinFactsByCertNo/{0}".format(cert_number)
         result = requests.get(request_url, headers={'authorization': 'bearer ' + self.API_KEY})
         result.raise_for_status()
@@ -71,20 +72,40 @@ class CoinCollection:
     def __init__(self):
         # Initialize a list to hold all of the Coin objects
         self.collection: list[Coin] = []
+        self.current_index = 0
+        super().__init__()
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self) -> Coin:
+        try:
+            result = self.collection[self.current_index]
+        except IndexError:
+            raise StopIteration
+        self.current_index += 1
+        return result
+    
+    def __len__(self):
+        return self.collection.__len__()
+    
+    def __getitem__(self, i):
+        return self.collection[i]
+    
 
     def read_file(self, file_path: str):
+        ''' Reads a saved file into the collection. '''
         with open(file_path, "r") as file:
             # Read the file.
             obj_str = file.read()
 
     def dump_json(self, file_path="saved.txt"):
+        ''' Creates a file that can be used in the read_file() method. '''
         with open(file_path, "wa") as file:
             # Serialize and write objects to the file.
             for coin in self.collection:
                 file.write(coin.serialize())
 
     def add_coin(self, coin: Coin):
+        ''' Adds a coin into the collection. '''
         self.collection.append(coin)
-
-    def list(self):
-        return self.collection
